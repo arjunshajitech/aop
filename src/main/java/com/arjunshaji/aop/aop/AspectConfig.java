1402858260d8fb3a3741e5ade66c4e44a7ccfa59
ARJUN SHAJI <arjunshajiforpc@gmail.com>
2024-02-19 12:54:13 +0530

    -- updated

--- src/main/java/com/arjunshaji/aop/aop/AspectConfig.java
+++ src/main/java/com/arjunshaji/aop/aop/AspectConfig.java
@@ -2,7 +2,9 @@ package com.arjunshaji.aop.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,35 +15,51 @@ import org.springframework.stereotype.Component;
 public class AspectConfig {
 
 
-    @Before("execution(* com.arjunshaji.aop.aop..*.*(..))")
-    public void authorization() {
-        // authorization performs here
-        log.info("authorization performed..................");
-    }
+//    @Before("execution(* com.arjunshaji.aop.aop..*.*(..))")
+//    public void authorization() {
+//        // authorization performs here
+//        log.info("authorization performed..................");
+//    }
+//
+//    @Before("execution(* com.arjunshaji.aop.aop..*.*(..))")
+//    public void authentication() {
+//        // authentication performs here
+//        log.info("authentication performed...................");
+//    }
+//
+//    @Before("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
+//    public void beforeLogging() {
+//        log.info("order creation started............");
+//    }
+//
+//    @After("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
+//    public void afterLogging() {
+//        log.info("order creation completed............");
+//    }
+//
+//
+//    @Before("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
+//    public void jsonValidate(JoinPoint joinPoint) {
+//        log.info("validating json................");
+//        Object[] args = joinPoint.getArgs();
+//        for (Object arg : args) {
+//            System.out.println("amount : " + arg);
+//        }
+//    }
 
-    @Before("execution(* com.arjunshaji.aop.aop..*.*(..))")
-    public void authentication() {
-        // authentication performs here
-        log.info("authentication performed...................");
-    }
+    // aspect = jsonValidate
+    // pointcut = execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
+    // advice - before
+    // joinPoint - get metadata
+    // * any return type
+    // (..) any number of arguments
 
-    @Before("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
-    public void beforeLogging() {
-        log.info("order creation started............");
-    }
+//    @Around("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
+//    public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
+//        System.out.println("Before executing the order method...");
+//        Object sampleReturnValue = "SampleReturnValue";
+//        System.out.println("After executing the order method...");
+//        return sampleReturnValue;
+//    }
 
-    @After("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
-    public void afterLogging() {
-        log.info("order creation completed............");
-    }
-
-
-    @Before("execution(* com.arjunshaji.aop.aop.AspectController.order(..))")
-    public void jsonValidate(JoinPoint joinPoint) {
-        log.info("validating json................");
-        Object[] args = joinPoint.getArgs();
-        for (Object arg : args) {
-            System.out.println("amount : " + arg);
-        }
-    }
 }
--- src/main/java/com/arjunshaji/aop/aop/AspectController.java
+++ src/main/java/com/arjunshaji/aop/aop/AspectController.java
@@ -12,29 +12,20 @@ public class AspectController {
     /* assume order creation before payment process */
     @PostMapping("/order/{amount}")
     public void order(@PathVariable("amount") double amount) {
-        createOrder(amount);
-        saveOrderToDatabase();
+        // logging
+        // authentication and authorization
+        // json validation etc.............
+        // business logic
         log.info("order business logic.............");
     }
 
     @PostMapping("/payment")
     public void payment() {
+        // logging
+        // authentication and authorization
+        // json validation etc.............
+        // business logic
         log.info("payment business logic.........");
     }
 
-
-
-    private void saveOrderToDatabase() {
-        // logging
-        // authentication and authorization
-        // json validation etc.............
-        // business logic
-    }
-
-    private void createOrder(double amount) {
-        // logging
-        // authentication and authorization
-        // json validation etc.............
-        // business logic
-    }
 }
